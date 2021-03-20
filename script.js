6d777260f958611aaa691fbb0f60a0e07ed43c91
oussamakami <kamili.oussama1@gmail.com>
2021-03-20 12:09:38 +0100

    copy functionality
    add copy to clipboard on clicking on the HEX value

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const HEXMAP = "0123456789ABCDEF".split("");
 
 
 document.querySelector("button").onclick = generateHexVal;
+document.querySelector("span").onclick = NotificationSlide;
 
 function generateHexVal() {
   let randomVals = [],
@@ -29,4 +30,15 @@ function updateBackgroundColor(HEXVAL) {
 }
 function updateHexCode(HEXVAL) {
     document.getElementById("HexVal").innerText = HEXVAL;
+}
+function NotificationSlide() {
+  const NOTIFICATION = document.getElementById('Notification'),
+        HEXVALUE = document.querySelector('span').innerText;
+        HOLDER = document.getElementById('ValueHolder');
+  NOTIFICATION.style.top = "20px";
+  setTimeout(()=>{NOTIFICATION.style.top = "-8.5vh"}, 2000);
+  HOLDER.value = HEXVALUE;
+  HOLDER.select();
+  HOLDER.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 }
